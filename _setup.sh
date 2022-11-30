b3e4fd2feaafae7a6a71c6feb78e6a31fac56590
Wibisana Bramawidya <wibisana.bramawidya@gmail.com>
2022-11-30 19:51:43 +0900

    Update setup

--- _setup.sh
+++ _setup.sh
@@ -25,6 +25,71 @@ _install_zsh() {
   sed -i 's/^plugins=.*$/plugins=(git aws zsh-syntax-highlighting)/g' ~/.zshrc
 }
 
+_setup_keys() {
+  [[ ! -f ~/.ssh/id_rsa ]] && ssh-keygen -t rsa -b 4096
+  [[ ! $(gpg2 -k | grep -w uid) ]] && gpg2 --full-generate-key
+  GPG_KEY_ID=$(gpg2 --list-secret-keys --keyid-format=long | grep -m 1 sec | sed -n 's/^.*[a-z0-9]*\/\([A-Z0-9]*\).*/\1/p')
+}
+
+_setup_git() {
+  type -p curl >/dev/null || sudo apt install curl -y
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && sudo apt update \
+    && sudo apt install gh -y
+
+  GPG_KEY_ID=$(gpg2 --list-secret-keys --keyid-format=long | grep -m 1 sec | sed -n 's/^.*[a-z0-9]*\/\([A-Z0-9]*\).*/\1/p')
+  read -p 'Git email:' GIT_EMAIL
+
+  git config --global user.name "Wibisana Bramawidya"
+  git config --global user.email ${GIT_EMAIL}
+  git config --global user.signingkey ${GPG_KEY_ID}
+  git config --global commit.gpgsign true
+
+  gpg2 --armor --export ${GPG_KEY_ID}
+
+  [ -f ~/.zshrc ] && echo >> ~/.zshrc
+  [ -f ~/.zshrc ] && echo 'export GPG_TTY=$(tty)' >> ~/.zshrc
+}
+
+_install_vim() {
+  sudo apt install -y vim
+
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  mv vim/.vimrc ${HOME}/.vimrc
+
+  env SHELL=(which sh) vim +BundleInstall! +BundleClean +qall
+
+  cat>>~/.zshrc <<EOF
+
+function updatevim
+    set -lx SHELL (which sh)
+    vim +BundleInstall! +BundleClean +qall
+end
+EOF
+}
+
+_install_vscode() {
+  wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
+  sudo dpkg -i vscode.deb
+
+  code --install-extension eamodio.gitlens
+  code --install-extension GitHub.vscode-pull-request-github
+  code --install-extension ms-vscode-remote.remote-ssh
+  code --install-extension golang.Go
+  code --install-extension ms-python.python
+  code --install-extension ms-toolsai.jupyter
+  code --install-extension HashiCorp.terraform
+  code --install-extension ms-kubernetes-tools.vscode-kubernetes-tools
+  code --install-extension ms-azuretools.vscode-docker
+  code --install-extension ms-vscode-remote.remote-containers
+  code --install-extension ms-vscode.makefile-tools
+  code --install-extension twxs.cmake
+  code --install-extension ms-vscode-remote.remote-ssh-edit
+
+  rm vscode.deb
+}
 
 _install_aws() {
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
@@ -77,35 +142,13 @@ EOF
   asdf plugin-add vault https://github.com/asdf-community/asdf-hashicorp.git
 }
 
-_setup_keys() {
-  [[ ! -f ~/.ssh/id_rsa ]] && ssh-keygen -t rsa -b 4096
-  [[ ! $(gpg2 -k | grep -w uid) ]] && gpg2 --full-generate-key
-  GPG_KEY_ID=$(gpg2 --list-secret-keys --keyid-format=long | grep -m 1 sec | sed -n 's/^.*[a-z0-9]*\/\([A-Z0-9]*\).*/\1/p')
-  gpg2 --armor --export ${GPG_KEY_ID}
-}
+_setup_aliases() {
+  echo >> ~/.zshrc
+  echo "alias sudo='sudo '" >> ~/.zshrc
+  echo "alias v='vim'" >> ~/.zshrc
+  echo "alias gcmm='git commit -m'" >> ~/.zshrc
 
-_setup_git() {
-  type -p curl >/dev/null || sudo apt install curl -y
-  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-    && sudo apt update \
-    && sudo apt install gh -y
-
-  GPG_KEY_ID=$(gpg2 --list-secret-keys --keyid-format=long | grep -m 1 sec | sed -n 's/^.*[a-z0-9]*\/\([A-Z0-9]*\).*/\1/p')
-  read -p 'Git email:' GIT_EMAIL
-
-  git config --global user.name "Wibisana Bramawidya"
-  git config --global user.email ${GIT_EMAIL}
-  git config --global user.signingkey ${GPG_KEY_ID}
-  git config --global commit.gpgsign true
-
-  [ -f ~/.zshrc ] && echo "\n"'export GPG_TTY=$(tty)' >> ~/.zshrc
-}
-
-_install_vscode() {
-  wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O vscode.deb
-  sudo dpkg -i vscode.deb
-
-  rm vscode.deb
+  mv aliases/.kubectl_aliases && echo "source ~/.kubectl_aliases" >> ~/.zshrc
+  mv aliases/.terraform_aliases && echo "source ~/.terraform_aliases" >> ~/.zshrc
+  mv aliases/.terragrunt_aliases && echo "source ~/.terragrunt_aliases" >> ~/.zshrc
 }
